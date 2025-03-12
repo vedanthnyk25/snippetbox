1caa9c2af13df33d32a9913416f52f76c7e266a8
vedanthnyk25 <vedanthnayak25@gmail.com>
2025-03-13 01:16:54 +0530

    add MySQL database connection and update go.mod dependencies

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,24 +6,51 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"database/sql"
+  _ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	
 	addr := flag.String("addr", ":4000", "http network address")
+    
+    dsn:= flag.String("dsn", "web:vedanthN@25@/snippetbox?parseTime=true", "MySQL data source name")
 
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+    
+	db, err:= openDB(*dsn)
+
+	if err!=nil{
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+    //always close the database connection
+	defer db.Close()
+
 
 	app := &application{
 		logger: logger,
 	}
 	logger.Info("Starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil,err
+	}
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
 }
 
 type application struct {
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module vedanth.snippetbox.net
 
 go 1.24.0
+
+require github.com/go-sql-driver/mysql v1.9.0
+
+require filippo.io/edwards25519 v1.1.0 // indirect
