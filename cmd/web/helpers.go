8cdc3341206b4cc83617aebd8872c34bdca49186
vedanthnyk25 <vedanthnayak25@gmail.com>
2025-05-22 22:54:13 +0530

    feat: implement user authentication middleware and update user existence check in database

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -59,7 +59,11 @@ func (app *application) newTemplateData(r *http.Request) TemplateData{
 }
 
 func (app *application) isAuthenticated(r *http.Request) bool{
-	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
+	isAuthenticated, ok:= r.Context().Value(isAuthenticatedContextKey).(bool)
+	if !ok{
+		return false
+	}
+	return isAuthenticated
 }
 
 
