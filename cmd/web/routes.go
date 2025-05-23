7a6d1d9b3ee0df24868bbcf41ab10b291d838e2d
vedanthnyk25 <vedanthnayak25@gmail.com>
2025-05-23 19:08:35 +0530

    feat: update static file handling and embed file system for templates

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"net/http"
-    "github.com/justinas/alice"
+
+	"github.com/justinas/alice"
+	"vedanth.snippetbox.net/ui"
 )
 
 func (app * application) routes() http.Handler{
     mux:= http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-    mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
     dynamic:= alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
