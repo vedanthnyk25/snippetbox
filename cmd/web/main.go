8833f2f64b8797bdd9f82d06c0d6a4ded5bad0b9
vedanthnyk25 <vedanthnayak25@gmail.com>
2025-05-24 15:42:45 +0530

    update main.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"flag"
-	//"log"
 	"crypto/tls"
 	"database/sql"
+	"fmt"
 	"html/template"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
-	"fmt"
 
 	"github.com/alexedwards/scs/mysqlstore" // New import
 	"github.com/alexedwards/scs/v2"
@@ -20,46 +19,41 @@ import (
 )
 
 func main() {
-	
-	addr := flag.String("addr", ":4000", "http network address")
-    
-  dsn := flag.String("dsn", fmt.Sprintf(
-    "%s:%s@tcp(%s:%s)/%s?parseTime=true",
-    os.Getenv("DB_USER"),
-    os.Getenv("DB_PASS"),
-    os.Getenv("DB_HOST"),
-    os.Getenv("DB_PORT"),
-    os.Getenv("DB_NAME"),
-), "MySQL data source name")
+	//addr := flag.String("addr", ":4000", "http network address")
+
+	dsn := flag.String("dsn", fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	), "MySQL data source name")
 
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-    
-	db, err:= openDB(*dsn)
 
-	if err!=nil{
+	db, err := openDB(*dsn)
+	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
-    //always close the database connection
 	defer db.Close()
 
-	//initialise template cache
-	templateCache, err:= newTemplateCache()
-	if err!=nil{
+	templateCache, err := newTemplateCache()
+	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
-    
-	formDecoder:= form.NewDecoder()
-	sessionManager:= scs.New()
+
+	formDecoder := form.NewDecoder()
+	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 
-	sessionManager.Cookie.Secure= true
-
-	tlsConfig:= &tls.Config{
+	tlsConfig := &tls.Config{
 		CurvePreferences: []tls.CurveID{
 			tls.X25519,
 			tls.CurveP256,
@@ -67,34 +61,59 @@ func main() {
 	}
 
 	app := &application{
-		logger: logger,
-		snippets: &models.SnippetModel{DB: db},
-		users: &models.UserModel{DB: db},
-		templateCache: templateCache,
-		formDecoder: formDecoder,
+		logger:         logger,
+		snippets:       &models.SnippetModel{DB: db},
+		users:          &models.UserModel{DB: db},
+		templateCache:  templateCache,
+		formDecoder:    formDecoder,
 		sessionManager: sessionManager,
 	}
-    srv:= &http.Server{
-		Addr: *addr,
-		Handler: app.routes(),
-		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
-		TLSConfig: tlsConfig,
 
-		IdleTimeout: time.Minute,
-		ReadTimeout: 5 * time.Second,
+	// Use PORT env var (Render sets this)
+	port := os.Getenv("PORT")
+	if port == "" {
+		// default local port
+		port = "4000"
+	}
+	addrStr := ":" + port
+
+	srv := &http.Server{
+		Addr:         addrStr,
+		Handler:      app.routes(),
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
 
-	logger.Info("Starting server on", "port",  srv.Addr)
-	err= srv.ListenAndServeTLS("./tls/localhost.pem", "./tls/localhost-key.pem")
-	logger.Error(err.Error())
-	os.Exit(1)
+	logger.Info("Starting server on port", "port", port)
+
+	// Check if TLS cert files exist - if yes, start TLS server, else plain HTTP
+	if _, err := os.Stat("./tls/localhost.pem"); err == nil {
+		if _, err := os.Stat("./tls/localhost-key.pem"); err == nil {
+			logger.Info("Using TLS certificates, starting HTTPS server")
+			err = srv.ListenAndServeTLS("./tls/localhost.pem", "./tls/localhost-key.pem")
+			if err != nil {
+				logger.Error(err.Error())
+				os.Exit(1)
+			}
+			return
+		}
+	}
+
+	logger.Info("TLS certificates not found, starting HTTP server")
+	err = srv.ListenAndServe()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
 }
 
 func openDB(dsn string) (*sql.DB, error) {
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 	err = db.Ping()
 	if err != nil {
@@ -105,10 +124,10 @@ func openDB(dsn string) (*sql.DB, error) {
 }
 
 type application struct {
-	logger *slog.Logger
-	snippets *models.SnippetModel
-	users *models.UserModel
-	templateCache map[string]*template.Template
-	formDecoder *form.Decoder
+	logger         *slog.Logger
+	snippets       *models.SnippetModel
+	users          *models.UserModel
+	templateCache  map[string]*template.Template
+	formDecoder    *form.Decoder
 	sessionManager *scs.SessionManager
 }
